567a896c3014bc3133c4b9af18f310b1d56d32bb
Dima Denisenko <dev@dima.best>
2016-12-29 17:08:35 +0200

    Work.

--- component/backend/config/backend/layout_updates.global.php
+++ component/backend/config/backend/layout_updates.global.php
@@ -5,9 +5,10 @@
  * @copyright  Copyright (c) 2016 WellCart Development Team    http://wellcart.org/
  * @license    http://www.opensource.org/licenses/BSD-3-Clause New BSD License
  */
+
 return [
     'layout_updates' => [
-        'backend/wellcart-backend-ui' => [
+        'backend/theme/wellcart-backend-ui' => [
             'ui/grid/standard' => [
                 'blocks' => [
                     'page.header.head' => [
