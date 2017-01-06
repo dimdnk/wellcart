45730e945d735e100aef375ae29ef8cac1edd7ca
Dima Denisenko <dev@dima.best>
2017-01-06 13:50:45 +0200

    Create listener for building form layout declarative.

---
+++ module/cms/config/common/forms.input_filter_specs.global.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * WellCart Platform
+ *
+ * @copyright  Copyright (c) 2017 WellCart Development Team    http://wellcart.org/
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+return [
+  'input_filter_specs' =>
+    [
+
+    ]
+];
