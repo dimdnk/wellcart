4a30f239905091dd0e8eed3d161cc26258305a12
Dima Denisenko <dev@dima.best>
2017-02-20 12:25:49 +0200

    Add skeleton for Directory controller tests

---
+++ module/directory/tests/unit/Controller/Backend/GeoZonesControllerTest.php
@@ -0,0 +1,128 @@
+<?php
+namespace WellCart\Directory\Test\Unit\Controller\Backend;
+
+use WellCart\Directory\Controller\Backend\GeoZonesController;
+
+class GeoZonesControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var GeoZonesController
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new GeoZonesController();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @todo   Implement testListAction().
+     */
+    public function testListAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testCreateAction().
+     */
+    public function testCreateAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testHandleForm().
+     */
+    public function testHandleForm()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testUpdateAction().
+     */
+    public function testUpdateAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testDeleteAction().
+     */
+    public function testDeleteAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testGroupActionHandlerAction().
+     */
+    public function testGroupActionHandlerAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testAttemptToPersistEntity().
+     */
+    public function testAttemptToPersistEntity()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testAttemptToDeleteEntity().
+     */
+    public function testAttemptToDeleteEntity()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testAttemptToPerformGroupAction().
+     */
+    public function testAttemptToPerformGroupAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
---
+++ module/directory/tests/unit/Controller/Console/UpdateCurrencyRatesControllerTest.php
@@ -0,0 +1,183 @@
+<?php
+namespace WellCart\Directory\Test\Unit\Controller\Console;
+
+use WellCart\Directory\Controller\Console\UpdateCurrencyRatesController;
+
+class UpdateCurrencyRatesControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UpdateCurrencyRatesController
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new UpdateCurrencyRatesController;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @todo   Implement testHandleAction().
+     */
+    public function testHandleAction()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testOnDispatch().
+     */
+    public function testOnDispatch()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement test__().
+     */
+    public function test__()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testSetTranslator().
+     */
+    public function testSetTranslator()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testGetTranslator().
+     */
+    public function testGetTranslator()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testHasTranslator().
+     */
+    public function testHasTranslator()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testSetTranslatorEnabled().
+     */
+    public function testSetTranslatorEnabled()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testIsTranslatorEnabled().
+     */
+    public function testIsTranslatorEnabled()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testSetTranslatorTextDomain().
+     */
+    public function testSetTranslatorTextDomain()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testGetTranslatorTextDomain().
+     */
+    public function testGetTranslatorTextDomain()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testSetLogger().
+     */
+    public function testSetLogger()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testGetLogger().
+     */
+    public function testGetLogger()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testSetObjectManager().
+     */
+    public function testSetObjectManager()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo   Implement testGetObjectManager().
+     */
+    public function testGetObjectManager()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
