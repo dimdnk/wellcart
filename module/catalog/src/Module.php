f08b0cd104654d2e09dc763f2970aab0cecb936b
Dima Denisenko <dev@dima.best>
2017-09-22 17:36:36 +0300

    Work.

--- module/catalog/src/Module.php
+++ module/catalog/src/Module.php
@@ -62,8 +62,8 @@ class Module implements
     public function getSetupMigrations(): array
     {
         return [
-            '20170907000000' => new Setup\Schema\Install(
-                '20170907000000'
+            '20170908000000' => new Setup\Schema\Install(
+                '20170908000000'
             ),
         ];
     }
@@ -76,8 +76,8 @@ class Module implements
     public function getSetupDataFixtures(): array
     {
         return [
-            '20170907000000' => new Setup\Data\Install(
-                '20170907000000'
+            '20170908000000' => new Setup\Data\Install(
+                '20170908000000'
             ),
         ];
     }
